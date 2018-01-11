7f46a2a827f61dc88f3bfff7aec803664a31bb71
ronraney <raneyron@yahoo.com>
2018-01-11 15:27:37 -0600

    Worked on navigation

--- pool/urls.py
+++ pool/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
--- pool/views.py
+++ pool/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 # Old school index
+@login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'blog/index.html')
