0722fdb81ef9f17097bf5c3b8594b17311811c49
ronraney <raneyron@yahoo.com>
2018-01-16 21:40:06 -0600

    Added some styles and notes about creating models

--- pool/models.py
+++ pool/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+# User Component - extend the built-in User model by creating a proxy model
+	## User-selected team name
+	## Email - username for login
+	## Password - password for login
+	## Point total
+	## Paid status
+
+# Teams Component	
+	## College
+	## Mascot
+	## Seeding
+	## Total wins 
+	## Total points - calculated incrementally
+	## Underdog wins
+	## Underdog bonus
+	
+# User Selections - one-to-one link? https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html
+	## User - authorized user only - personalized data
+	## Bracket name
+	## Round 1 array - CHOICES
+	## Round 2 array - CHOICES
+	## Round 3 array - CHOICES
+	## Round 4 array - CHOICES
+	## Round 5 array - CHOICES
+	## Round 6 array - CHOICES
+	
--- pool/views.py
+++ pool/views.py
@@ -6,3 +6,7 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'blog/index.html')
+	
+# User list view with point totals - links to detail view of selections
+# User detail view(s) with selections (available only after start date)
+# Selection form view(s) - 4x4 view and Final Four
